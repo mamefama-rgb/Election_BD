d6f9b2a527157c796191ec92f1fefbb294b98855
mamefama <mamefama@Maam-fama7.tgi.sn>
2025-02-14 22:49:12 +0000

    3rd commit

---
+++ src/app/inscriptions/inscriptions.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-inscriptions',
+  imports: [],
+  templateUrl: './inscriptions.component.html',
+  styleUrl: './inscriptions.component.css'
+})
+export class InscriptionsComponent {
+
+}
